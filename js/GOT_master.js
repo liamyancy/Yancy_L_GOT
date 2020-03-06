46f9701421408541cd5bacc16c47055ce81804dd
Yancy <l_yancy@ats-hr420-dm28.fc.ca>
2020-03-06 13:11:37 -0500

    updatingvideos

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -7,9 +7,22 @@
 
 	//functions go in the middle -> what do we want our app to do?
 	function showLightBox() {
+		//debugger;
+		//retrieve the css class that matches the mideo name in the video folder
+		let houseName = this.className.split(" ")[1];
+
+
+		//capitalize the first letter of the house name
+		//then add the rest of the house name to it
+		let newSource = houseName.charAt(0).toUpperCase() + houseName.slice(1);
+
+		let targetSource = `video/House-${newSource}.mp4`;
+		//debugger;
 		//show the lightbox ona click
 		lightBox.classList.add("show-lightbox");
 		//play video when it opens
+		houseVideo.src = targetSource;
+		houseVideo.load();
 		houseVideo.play();
 	}
 
